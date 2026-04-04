1e0d4785486fdc978b63f01376eab4a73c5e198b
keyushhh <keyushh@gmail.com>
2026-04-04 14:00:24 +0530

    android added

---
+++ android/app/src/main/java/com/gridpe/rider/MainActivity.java
@@ -0,0 +1,5 @@
+package com.gridpe.rider;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gridpe.rider',
+  appName: 'GridPeRider',
+  webDir: 'dist'
+};
+
+export default config;
